2e78c0df1d56c0a1cfcadc3937174e81d7d8c1f8
giorgiospedaledev <giorgiospedaledev@gmail.com>
2023-12-15 04:47:18 +0100

    finished all logic for sudoku solver, TODO: Fix the code so If the coordinate submitted to api/check does not point to an existing grid cell, the returned value will be { error: 'Invalid coordinate'}

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,24 +1,101 @@
-class SudokuSolver {
+const { puzzlesAndSolutions } = require("./puzzle-strings");
 
+class SudokuSolver {
   validate(puzzleString) {
+    if (!puzzleString) {
+      throw new Error("Required field missing");
+    }
+
+    if (!/^[\.1-9]*$/.test(puzzleString)) {
+      throw new Error("Invalid characters in puzzle");
+    }
+
+    if (puzzleString.length !== 81) {
+      throw new Error("Expected puzzle to be 81 characters long");
+    }
+
+    if (puzzleString.match(/[1-9]/g).length < 17) {
+      throw new Error("Puzzle cannot be solved");
+    }
+
+    return true;
   }
 
   checkRowPlacement(puzzleString, row, column, value) {
-
+    const rowString = puzzleString.slice(row * 9, row * 9 + 9);
+    return !rowString.includes(value);
   }
 
   checkColPlacement(puzzleString, row, column, value) {
-
+    const col = [];
+    for (let i = 0; i < 9; i++) {
+      col.push(puzzleString.charAt(9 * i + column));
+    }
+    return !col.includes(value);
   }
 
   checkRegionPlacement(puzzleString, row, column, value) {
+    const region = [];
+    const rowFloor = Math.floor(row / 3);
+    const colFloor = Math.floor(column / 3);
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        region.push(
+          puzzleString.charAt((rowFloor * 3 + i) * 9 + (colFloor * 3 + j))
+        );
+      }
+    }
 
+    return !region.includes(value);
   }
 
   solve(puzzleString) {
-    
+    const initialState = puzzleString;
+    let currentState = puzzleString;
+
+    function setCharAt(str, index, chr) {
+      if (index > str.length - 1) return str;
+      return str.substring(0, index) + chr + str.substring(index + 1);
+    }
+
+    let times = 0;
+    while (!/^[0-9]*$/.test(currentState) && times < 10) {
+      for (let i = 0; i < 9; i++) {
+        for (let j = 0; j < 9; j++) {
+          const index = i * 9 + j;
+          if (initialState.charAt(index) == ".") {
+            let possiblePlacementCount = 0;
+            let possibleValue;
+
+            for (let value = 1; value <= 9; value++) {
+              let canSet =
+                this.checkColPlacement(currentState, i, j, value.toString()) &
+                  this.checkRowPlacement(
+                    currentState,
+                    i,
+                    j,
+                    value.toString()
+                  ) &&
+                this.checkRegionPlacement(currentState, i, j, value.toString());
+              if (canSet) {
+                possiblePlacementCount++;
+                possibleValue = value.toString();
+              }
+            }
+            if (possiblePlacementCount === 1) {
+              currentState = setCharAt(currentState, index, possibleValue);
+            }
+            
+          }
+        }
+      }
+      times++;
+    }
+    if (times == 10) {
+      throw new Error("Puzzle cannot be solved")
+    }
+    return currentState;
   }
 }
 
 module.exports = SudokuSolver;
-
--- routes/api.js
+++ routes/api.js
@@ -8,11 +8,77 @@ module.exports = function (app) {
 
   app.route('/api/check')
     .post((req, res) => {
+       
+      try {
+      const puzzle = req.body.puzzle;
+      const coordinate = req.body.coordinate;
+      const value = req.body.value;
 
+      if (!puzzle || !coordinate || !value) 
+        return res.json({error: 'Required field(s) missing'})
+
+      const row = coordinate.charCodeAt(0) - 65;
+      const column = Number.parseInt(coordinate.match(/\d+/g)[0]) - 1;
+      const parsedValue = Number.parseInt(value);
+      
+
+      if (row < 0 || row > 8 || column < 0 || column > 8) {
+
+        console.log(row, column)
+        return res.json({ error: 'Invalid coordinate'})
+      }
+
+      if (isNaN(parsedValue) || parsedValue < 1 || parsedValue > 9 ) 
+        return res.json({error: 'Invalid value'})
+
+      solver.validate(puzzle)
+
+      if (puzzle.charAt(row * 9 + column) != ".") {
+        return res.json({valid: true})
+      }
+      
+      const canPlaceInRow = solver.checkRowPlacement(puzzle, row, column, value);
+      const canPlaceInColumn = solver.checkColPlacement(puzzle, row, column, value);
+      const canPlaceInRegion = solver.checkRegionPlacement(puzzle, row, column, value);
+
+      const response = {}
+
+      response.valid = canPlaceInRow && canPlaceInColumn && canPlaceInRegion
+      if (!response.valid) {
+        response.conflict = [];
+      }
+      if (!canPlaceInRow) {
+        response.conflict.push("row");
+      } 
+      if (!canPlaceInColumn) {
+        response.conflict.push("column");
+      } 
+      if (!canPlaceInRegion) {
+        response.conflict.push("region");
+      }
+
+      console.log(req.body);
+      console.log(response)
+      res.json(response);
+
+      }catch (e) {
+        res.json({error: e.message})
+      }
     });
     
   app.route('/api/solve')
     .post((req, res) => {
+      const puzzle = req.body.puzzle;
+      try {
+
+      solver.validate(puzzle)
+
+      res.json({solution: solver.solve(puzzle)})
+      
+
+    }catch (e) {
+      res.json({error: e.message})
+    }
 
     });
 };
